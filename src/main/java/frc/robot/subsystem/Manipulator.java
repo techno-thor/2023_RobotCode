b22e42c65584719249e192cae1d96c5b92e386fb
techno-thor <joejeakle@gmail.com>
2023-02-14 20:10:20 -0500

    Manipulator updated to use pneumatics

--- src/main/java/frc/robot/subsystem/Manipulator.java
+++ src/main/java/frc/robot/subsystem/Manipulator.java
@@ -19,37 +19,15 @@ import frc.robot.Robot;
  */
 @SuppressWarnings("all")
 public class Manipulator {
-    /** Preset Arm angles. */
-    public enum Position {
-        RETRACTED(0.0),
-        EXTENDED(115.0);
-
-        public final double angle;
-        private Position(double angle) {
-            this.angle = angle;
-        }
-    }
-
     //Subsystem NetworkTable
     private static final NetworkTable tblManipulator = Robot.tblSubsystem.getSubTable("Manipulator");
 
-    //Dashboard Entries
-    private static final Entry<Double> entArm_Angle = new Entry<Double>(tblManipulator, "Arm Angle");
-
-    //Motors
-    private static final WPI_TalonFX mtrArm = new WPI_TalonFX(7);
-
     //Solenois
-    private static final Solenoid solGrip = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
-
-    //Sensors TODO: Find out how they plan to limit arm if not pneumatics
-    private static final MagEncoder encArm = new MagEncoder(mtrArm);
-
-    //PID Controllers
-    private static final PIDController pidArm_Angle = new PIDController(0.0, 0.0, 0.0);
+    private static final Solenoid solArm = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
+    private static final Solenoid solGrip = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
 
     //Variables
-    private static double mArmPower = 0.0;
+    private static boolean mIsArmExtended = false;
     private static boolean mIsGripOpen = false;
 
     /** Private constructor, cannot instantiate. */
@@ -58,28 +36,6 @@ public class Manipulator {
     /** Initialize subsystem and all components. */
     public static void init() {
         Console.logMsg("Initializing Manipulator Subsystem...");
-
-        //Configure motor inversions
-        Console.logMsg("Configuring Motor inversions...");
-        mtrArm.setInverted(false);
-
-        //Configure motor NeutralModes
-        Console.logMsg("Configuring Motor NeutralModes...");
-        mtrArm.setNeutralMode(NeutralMode.Brake);
-
-        //Configure Sensors
-        Console.logMsg("Configuring Sensors...");
-        encArm.configDistancePerPulse(360.0 / 2048.0);
-
-        //Configure PIDs
-        Console.logMsg("Configuring PIDs...");
-        pidArm_Angle.setTolerance(5.0);
-        pidArm_Angle.configAtSetpointTime(0.125);
-        pidArm_Angle.configOutputRange(-1.0, 1.0);
-
-        //Reset Sensors
-        Console.logMsg("Resetting Sensor values...");
-        resetArmAngle();
 
         Console.logMsg("Manipulator Subsystem initialization complete.");
     }
@@ -91,63 +47,20 @@ public class Manipulator {
 
     /** Push new values to dashboard objects. */
     public static void pushDashboardValues() {
-        //Push Sensor values
-        entArm_Angle.set(getArmAngle());
-    }
-
-    /** Reset Arm encoder distance. */
-    public static void resetArmAngle() { encArm.reset(); }
-
-    /**
-     * Read the angle of the Arm since last reset.
-     * @return Angle in degrees.
-     */
-    public static double getArmAngle() { return encArm.getDistance(); }
-
-    /**
-     * Assign power to the Arm motor buffer.
-     * @param power [-1.0, 1.0] Power to the Arm
-     */
-    public static void setArmPower(double power) {
-        mArmPower = power;
-    }
-
-    /** Turn off the Arm motor. */
-    public static void disableArm() { setArmPower(0.0); }
-
-    /**
-     * Enable Arm Angle PID control of the Manipulator.
-     * @param angle Target angle in degrees.
-     */
-    public static void goToAngle(double angle) {
-        pidArm_Angle.setSetpoint(angle);
-        pidArm_Angle.enable();
+        
     }
 
     /**
-     * Enable Arm Angle PID control of the Manipulator.
-     * @param position Target angle via preset positions.
-     * 
-     * @see Manipulator.Position
+     * Read whether the Arm is extended.
+     * @return True if the Arm is set to extended.
      */
-    public static void goToAngle(Position position) {
-        goToAngle(position.angle);
-    }
+    public static boolean isArmExtended() { return mIsArmExtended; }
 
     /** Enable Arm Angle PID to extend the Arm. */
-    public static void extendArm() { goToAngle(Position.EXTENDED); }
+    public static void extendArm() { mIsArmExtended = true; }
 
     /** Enable Arm Angle PID to retract the Arm. */
-    public static void retractArm() { goToAngle(Position.RETRACTED); }
-
-    /**
-     * Read whether the the Arm Angle PID has reached its target angle.
-     * @return True if the Arm angle is on target.
-     */
-    public static boolean isAtAngle() { return pidArm_Angle.atSetpoint(); }
-
-    /** Disable Arm Angle PID control of the Manipulator. */
-    public static void disableAnglePID() { pidArm_Angle.disable(); }
+    public static void retractArm() { mIsArmExtended = false; }
 
     /**
      * Read whether the Grip is open.
@@ -161,27 +74,15 @@ public class Manipulator {
     /** Close the Grip. */
     public static void closeGrip() { mIsGripOpen = false; }
 
-    /** Disable all PID control of the Manipulator. */
-    public static void disablePIDs() {
-        disableAnglePID();
-    }
-
-    /** Disable all PID control of the Maipulator and stop the Arm motor. */
+    /** Placeholder to disable all components. */
     public static void disable() {
-        disablePIDs();
-        disableArm();
+        
     }
 
     /** Update subsystem and all components. */
     public static void periodic() {
-        //PID Override
-        if(pidArm_Angle.isEnabled())
-            setArmPower(pidArm_Angle.calculate(getArmAngle()));
-
-        //Safety Checks
-
         //Apply buffers
-        mtrArm.set(mArmPower);
+        solArm.set(mIsArmExtended);
         solGrip.set(mIsGripOpen);
     }
 }
